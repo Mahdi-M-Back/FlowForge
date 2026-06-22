4393644cf952a79a2552505b7e92facba01e1dc7
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-06-22 22:19:01 +0330

    .

--- router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const userRouter = require('./modules/user/router/user.route');
-
-const router = express.Router();
-
-router.use('/users', userRouter);
-
-module.exports = router;
