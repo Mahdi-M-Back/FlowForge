f365b69b12f74b89a2c4c5053d9f8ecb6f54672c
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-06 21:11:34 +0330

    refactor: remove dotenv configuration and related console log from app setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,7 @@
 import express from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const app = express();
 
-console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 app.use(express.json());
 
 
