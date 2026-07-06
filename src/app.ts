2be7a9859e2d7a4214435d3e12eb3b0161461f2e
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-06 23:54:56 +0330

    feat: add router integration to Express app setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import { createRouter } from './routes/index.js';
 
 const app = express();
 
 app.use(express.json());
 
+app.use(createRouter())
 
 export default app;
