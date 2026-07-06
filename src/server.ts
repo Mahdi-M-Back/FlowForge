a6e25a34259fdd1052285d838c497423240bdce3
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-06 23:53:06 +0330

    feat: enhance server setup with database connection and error handling

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,18 @@
-import app from './app.js';
+import "dotenv/config";
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}..🚀`);
-});
+import app from './app.js';
+import { env } from "./config/env.js";
+import { createDatabase } from "./config/database.js";
+
+
+try{
+  await createDatabase(env.database.url)
+  const PORT = env.server.port || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}..🚀`)
+  });
+}catch(err){
+  console.error(err)
+  process.exit(1)
+};
+
