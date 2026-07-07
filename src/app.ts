5dee8f1659052410190cdddb9a21654221f4676b
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-07 23:36:21 +0330

    fix: update router import and API endpoint configuration in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import { createRouter } from './routes/index.js';
+import express from "express";
+import router from "./routes/index.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(createRouter())
+app.use("/api", router);
 
 export default app;
