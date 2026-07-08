4d51263a92883dd4f7cd1d4bd3551dcddcb2306f
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-08 23:58:30 +0330

    feat: add find function to retrieve user by email

--- src/modules/auth/repository.ts
+++ src/modules/auth/repository.ts
@@ -15,6 +15,20 @@ async function create(data: RegisterDto) {
   return result;
 }
 
+async function find(email: string) {
+  const result = await pool.query(
+    `
+    SELECT *
+    FROM users
+    WHERE email = $1
+    `,
+    [email],
+  );
+
+  return result.rows[0] ?? null;
+}
+
 export default {
   create,
+  find,
 };
