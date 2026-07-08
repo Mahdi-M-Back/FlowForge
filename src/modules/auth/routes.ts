48b80c6a6edbb92ea3bb2f466a05d0dd184f0f62
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-08 23:57:23 +0330

    feat: add login route to authentication router

--- src/modules/auth/routes.ts
+++ src/modules/auth/routes.ts
@@ -4,5 +4,6 @@ import * as controller from "./controller.js";
 const router = Router();
 
 router.post("/signup", controller.signup);
+router.post("/login", controller.login);
 
 export default router;
