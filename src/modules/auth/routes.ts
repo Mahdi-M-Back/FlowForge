ca685c2fa6797562ae931936226c9142f6b5a0ef
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-09 16:12:19 +0330

    feat: add refresh token route to handle token refresh requests

--- src/modules/auth/routes.ts
+++ src/modules/auth/routes.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.post("/signup", controller.signup);
 router.post("/login", controller.login);
 router.post("/logout", controller.logout);
+router.post("/refresh-token", controller.refreshToken);
 
 export default router;
