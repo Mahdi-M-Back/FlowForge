f5c33cbbb1a474525af775150efe946e1aa79c6f
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-10 23:13:35 +0330

    refactor: enhance token generation and protection logic in jwt.ts

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -22,14 +22,44 @@ function generateTokens(userId: string, res: any) {
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in ms
   });
 
-  return { access , refresh };
+  return { access, refresh };
 }
-function generateAccessTokens(userId: string) {
-  const access = accessToken(userId);
-  return { access };
+
+function generateAccessTokens(userId: string, refreshToken: string) {
+  try {
+    jwt.verify(refreshToken, env.jwt.refreshTokenSecret) as jwt.JwtPayload;
+    const access = accessToken(userId);
+
+    return { access };
+  } catch (err) {
+    return false;
+  }
 }
+
+function protect(req: any, res: any, next: any) {
+  if (!req.headers.authorization || !req.headers.authorization.startsWith("Bearer")) {
+    return res.status(401).json({
+      status: "faild",
+      data: "Unauthorized access.",
+    });
+  }
+  let decoded: any;
+  const token = req.headers.authorization.split(" ")[1];
+  try {
+    decoded = jwt.verify(token, env.jwt.accessTokenSecret) ;
+  } catch (error) {
+    return res.status(401).json({
+      status: "faild",
+      data: "You are not logged in. Please log in to get access.",
+    });
+  }
+  req.userId = decoded.userId;
+  next();
+}
+
 
 export default {
   generateTokens,
   generateAccessTokens,
+  protect,
 };
