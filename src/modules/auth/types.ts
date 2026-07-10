4413e507e467e5310e3b5ec8abe925108e120ff0
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-10 23:14:41 +0330

    feat: add User interface properties for email, role, and timestamps

--- src/modules/auth/types.ts
+++ src/modules/auth/types.ts
@@ -7,4 +7,10 @@ export interface User {
   id: string;
   name: string;
   password: string;
+  email: string;
+  role: "user" | "admin" | "owner",
+  is_deleted: boolean;
+  created_at: Date;
+  updated_at: Date | null;
+  deleted_at: Date | null;
 }
