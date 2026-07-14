8a5bb71fc148d2d5be6f15ae561981afb2665e1e
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-14 19:59:07 +0330

    refactor: format createWorkspace function and add getOne function to workspace service

--- src/modules/workspace/service.ts
+++ src/modules/workspace/service.ts
@@ -2,7 +2,7 @@ import repository from "./repository.js";
 import type { WorkspaceDto } from "./schema.js";
 
 async function createWorkspace(data: WorkspaceDto, userId: string) {
-  const result = await repository.createWorkspace(data,userId);
+  const result = await repository.createWorkspace(data, userId);
   return result;
 }
 
@@ -11,7 +11,16 @@ async function getAllWorkspaces() {
   return result;
 }
 
+async function getOne(params: string, owner_id: string) {
+  const result = await repository.getOne(params);
+  if (!(owner_id === result.owner_id)) {
+    return false;
+  }
+  return result.name;
+}
+
 export default {
   createWorkspace,
   getAllWorkspaces,
-}
+  getOne,
+};
