51172d7dcfb78877238d22813c22424099035db7
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-18 23:50:13 +0330

    feat: implement createMembership function for inserting new memberships

---
+++ src/modules/membership/repository.ts
@@ -0,0 +1,17 @@
+import pool from "@/config/database.js";
+
+async function createMembership(workspace_id: string, body: object) {
+  const result = await pool.query(
+    `
+    INSERT INTO membership(user_id,workspace_id,role)
+    VALUES($1,$2,$3)
+    RETURNING 
+    id,
+    user_id,
+    workspace_id,
+    role
+    `,
+    [body.user_id, workspace_id, body.role],
+  );
+  return result.rows[0] ?? null;
+}
