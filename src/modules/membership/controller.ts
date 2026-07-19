06252410c57d99f68b7103880cd0fc5836ef7cbc
MAHDI-M-BACK <mahdim.back@gmail.com>
2026-07-19 21:37:57 +0330

    feat: add getAllMembership function to retrieve all memberships

--- src/modules/membership/controller.ts
+++ src/modules/membership/controller.ts
@@ -18,3 +18,17 @@ export async function createMembership(req: Request, res: Response) {
     data: createMembership,
   });
 }
+
+export async function getAllMembership(req: Request, res: Response) {
+  const result = await service.getAll(req.params.id);
+  if (!result) {
+    return res.status(404).json({
+      status: "Faild",
+      data: "Can not find any Membership",
+    });
+  }
+  return res.status(200).json({
+    status: "Success",
+    data: result,
+  });
+}
